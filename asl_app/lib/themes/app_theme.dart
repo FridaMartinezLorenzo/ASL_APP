bbbfaf8002fe0d122e0abb8e278a067a869bc4c3
JorgeAEV <127627359+JorgeAEV@users.noreply.github.com>
2025-06-12 03:31:26 -0600

    Añadiendo themes para una plantilla de diseño más facil de modificar

---
+++ asl_app/lib/themes/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.background,
+      appBarTheme: AppBarTheme(
+        color: AppColors.primary,
+        foregroundColor: Colors.white,
+        elevation: 2,
+      ),
+      textTheme: const TextTheme(
+        bodyMedium: TextStyle(fontSize: 16, color: AppColors.textPrimary),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.primary,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        ),
+      ),
+    );
+  }
+}
