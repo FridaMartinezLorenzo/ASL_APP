f3324e29cae65d518b9a2958166517d654f8e525
JorgeAEV <127627359+JorgeAEV@users.noreply.github.com>
2025-06-12 04:58:48 -0600

    corregido gestos especiales

--- asl_app/lib/data/asl_special_gestures_data.dart
+++ asl_app/lib/data/asl_special_gestures_data.dart
@@ -3,21 +3,21 @@ import 'package:asl_app/models/special_gesture_info.dart';
 final List<SpecialGestureInfo> aslSpecialGesturesData = [
   SpecialGestureInfo(
     name: 'Gracias',
-    imagePath: 'assets/images/special_gestures/gracias.png',
-    videoPath: 'assets/videos/special_gestures/gracias.mp4',
+    imagePath: 'assets/images/gracias.png',
+    videoPath: 'assets/videos/gracias.mp4',
     description: 'Este gesto representa gratitud, se realiza moviendo la mano desde el mentón hacia adelante.',
   ),
   SpecialGestureInfo(
     name: 'Sí',
-    imagePath: 'assets/images/special_gestures/si.png',
-    videoPath: 'assets/videos/special_gestures/si.mp4',
+    imagePath: 'assets/images/si.png',
+    videoPath: 'assets/videos/si.mp4',
     description: 'Gesto para afirmar o decir que sí, generalmente con el pulgar hacia arriba o moviendo la cabeza.',
   ),
   SpecialGestureInfo(
     name: 'No',
-    imagePath: 'assets/images/special_gestures/no.png',
-    videoPath: 'assets/videos/special_gestures/no.mp4',
+    imagePath: 'assets/images/no.png',
+    videoPath: 'assets/videos/no.mp4',
     description: 'Gesto para negar o decir que no, realizado moviendo los dedos índice y medio juntos hacia delante y atrás.',
   ),
-  // Añade más gestos especiales aquí...
+
 ];
