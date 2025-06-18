78daf77ff5552e6df6611e318f905886bf71cb99
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2025-06-18 00:39:09 -0600

    FInal release

--- asl_app/lib/providers/lsm_provider.dart
+++ asl_app/lib/providers/lsm_provider.dart
@@ -7,22 +7,23 @@ import 'package:http/http.dart' as http;
 import '../models/sign_model.dart';
 
 class LSMProvider with ChangeNotifier {
- String getBaseUrl() {
+  String getBaseUrl() {
     if (kIsWeb) {
       return 'http://127.0.0.1:8080'; // Navegador web
     } else if (Platform.isAndroid) {
       // Para emulador Android:
-      // return 'http://10.0.2.2:8080'; 
+      // return 'http://10.0.2.2:8080';
 
       // Para dispositivo Android físico en red local:
-      return 'http://18.117.3.175:8080';
+      //return 'http://18.117.3.175:8080';
+      //Con ngrok
+      return 'https://fast-unique-hyena.ngrok-free.app';
     } else if (Platform.isIOS) {
       return 'http://localhost:8080'; // Emulador iOS
     } else {
       return 'http://localhost:8080'; // Otros (desktop, etc)
     }
   }
-
 
   bool isLoading = false;
   SignResponse? lastResponse;
@@ -32,8 +33,16 @@ class LSMProvider with ChangeNotifier {
       isLoading = true;
       notifyListeners();
 
-      final request = http.MultipartRequest('POST', Uri.parse('${getBaseUrl()}/detectar'));
+      final request = http.MultipartRequest(
+        'POST',
+        Uri.parse('${getBaseUrl()}/detectar'),
+      );
+
+      // Añadir archivo
       request.files.add(await http.MultipartFile.fromPath('file', imagen.path));
+
+      // ✅ Añadir el header que evita la advertencia de ngrok
+      request.headers['ngrok-skip-browser-warning'] = 'true';
 
       final response = await request.send();
       final responseBody = await response.stream.bytesToString();
